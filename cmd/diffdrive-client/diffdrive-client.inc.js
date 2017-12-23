fab949b989e6b7f40891447a312c9e7b4e6b35e1
Jacques Michiels <jacquesmichiels@gmail.com>
2017-12-23 17:08:41 +0100

    configured viewbox

--- cmd/diffdrive-client/diffdrive-client.inc.js
+++ cmd/diffdrive-client/diffdrive-client.inc.js
@@ -1,76 +1,75 @@
-// Distance between center of wheels.
-const WheelsSpacing = 250;
+// Distance between center of wheels (m).
+const WheelsSpacing = 0.25;
 
-// Width of the wheels.
-const WheelWidth = 35;
+// Radius of the wheels (m).
+const WheelRadius = 0.035;
 
-// Radius of the wheels.
-const WheelRadius = 40;
+// Generate and append the robot vizualisation to the page body.
+const robotSVG = AppendRobotSVG(WheelsSpacing, WheelRadius, 0.02, 0.005, 0.005);
 
-// Distance between the body and the wheel.
-let Margin = 5;
+function AppendRobotSVG(WheelsSpacing, WheelRadius,WheelWidth, Margin, Stroke) {
 
-// Chassis stroke thickness.
-const Stroke = 5;
+    // Increase margin to take stroke in consideration.
+    Margin += Stroke;
 
+    // Width of the wheel.
+    const LxWheel = WheelWidth;
 
+    // Length of the wheel.
+    const LyWheel = 2 * WheelRadius;
 
-// Increase margin to take stroke in consideration.
-Margin += Stroke;
+    // Width of the wheel well.
+    const LxWell = WheelWidth + Margin;
 
-// Width of the wheel.
-const LxWheel = WheelWidth;
+    // Length of the wheel well.
+    const LyWell = 2 * (WheelRadius + Margin);
 
-// Length of the wheel.
-const LyWheel = 2 * WheelRadius;
+    // Width of the chassis.
+    const LxChassis = WheelsSpacing + WheelWidth;
 
-// Width of the wheel well.
-const LxWell = WheelWidth + Margin;
+    // Compute chassis radius.
+    const RChassis = Math.sqrt((LxChassis / 2) * (LxChassis / 2) + (LyWell / 2) * (LyWell / 2));
 
-// Length of the wheel well.
-const LyWell = 2 * (WheelRadius + Margin);
+    // Append SVG container.
+    const svgContainer = d3.select("body").append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 3 0.001");
 
-// Width of the chassis.
-const LxChassis = WheelsSpacing + WheelWidth;
+    // Create a group for the robot.
+    const group = svgContainer.append("g")
+        .attr("transform", "translate(0.5,0.5)");
 
-// Compute chassis radius.
-const RChassis = Math.sqrt((LxChassis/2)*(LxChassis/2)+(LyWell/2)*(LyWell/2));
-
-// Append SVG container.
-const svgContainer = d3.select("body").append("svg");
-
-// Create a group for the robot.
-const group = svgContainer.append("g")
-    .attr("transform", "translate(400,200)");
-
-// Append chassis path (gray).
-group.append("path")
-    .attr("stroke", "black")
-    .attr("stroke-width", Stroke)
-    .attr("stroke-linejoin", "round")
-    .attr("fill", "lightgray")
-    .attr("d",
-        `M ${-LxChassis / 2} ${-LyWell / 2} ` +
-        `a ${RChassis} ${RChassis} 0 0 1 ${LxChassis} 0 ` +
-        `l ${-LxWell} 0 l 0 ${LyWell} l ${LxWell} 0 ` +
-        `a ${RChassis} ${RChassis} 0 0 1 ${-LxChassis} 0 ` +
-        `l ${LxWell} 0 l 0 ${-LyWell} ` +
-        "Z");
-
-const appendWheelRectangle = function (x, color) {
-    group.append("rect")
-        .attr("fill", color)
+    // Append chassis path (gray).
+    group.append("path")
         .attr("stroke", "black")
         .attr("stroke-width", Stroke)
         .attr("stroke-linejoin", "round")
-        .attr("width", LxWheel)
-        .attr("height", LyWheel)
-        .attr("x", x - LxWheel / 2)
-        .attr("y", -LyWheel / 2);
-};
+        .attr("fill", "lightgray")
+        .attr("d",
+            `M ${-LxChassis / 2} ${-LyWell / 2} ` +
+            `a ${RChassis} ${RChassis} 0 0 1 ${LxChassis} 0 ` +
+            `l ${-LxWell} 0 l 0 ${LyWell} l ${LxWell} 0 ` +
+            `a ${RChassis} ${RChassis} 0 0 1 ${-LxChassis} 0 ` +
+            `l ${LxWell} 0 l 0 ${-LyWell} ` +
+            "Z");
 
-// Append left wheel rectangle (red).
-appendWheelRectangle(-(LxChassis - LxWheel) / 2, "red");
+    const appendWheelRectangle = function (x, color) {
+        group.append("rect")
+            .attr("fill", color)
+            .attr("stroke", "black")
+            .attr("stroke-width", Stroke)
+            .attr("stroke-linejoin", "round")
+            .attr("width", LxWheel)
+            .attr("height", LyWheel)
+            .attr("x", x - LxWheel / 2)
+            .attr("y", -LyWheel / 2);
+    };
 
-// Append right wheel rectangle (blue).
-appendWheelRectangle((LxChassis - LxWheel) / 2, "blue");
+    // Append left wheel rectangle (red).
+    appendWheelRectangle(-(LxChassis - LxWheel) / 2, "red");
+
+    // Append right wheel rectangle (blue).
+    appendWheelRectangle((LxChassis - LxWheel) / 2, "blue");
+
+    return group;
+}
