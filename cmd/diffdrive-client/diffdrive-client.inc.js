d6f67058d7910b89b51ec52b4fc7b5e5b73cd4d0
Jacques Michiels <jacquesmichiels@gmail.com>
2018-01-28 14:57:14 -0500

    working on the updating

--- cmd/diffdrive-client/diffdrive-client.inc.js
+++ cmd/diffdrive-client/diffdrive-client.inc.js
@@ -1,75 +1,117 @@
-// Distance between center of wheels (m).
-const WheelsSpacing = 0.25;
+let viewbox = {
+    // Pixels by meters.
+    scale: 1000,
+    // Center of the viewbox (meters).
+    center: {x: 0, y: 0},
+};
 
-// Radius of the wheels (m).
-const WheelRadius = 0.035;
+const svg = d3.select('body').append('svg')
+    .attr('preserveAspectRatio', 'xMinYMin slice');
 
-// Generate and append the robot vizualisation to the page body.
-const robotSVG = AppendRobotSVG(WheelsSpacing, WheelRadius, 0.02, 0.005, 0.005);
 
-function AppendRobotSVG(WheelsSpacing, WheelRadius,WheelWidth, Margin, Stroke) {
+function update() {
 
-    // Increase margin to take stroke in consideration.
-    Margin += Stroke;
+    svg.data([viewbox]);
 
-    // Width of the wheel.
-    const LxWheel = WheelWidth;
+    // Update SVG viewbox.
+    svg.attr('viewBox', function (data) {
 
-    // Length of the wheel.
-    const LyWheel = 2 * WheelRadius;
+        // Size of the SVG element (in pixels).
+        const {width, height} = this.getBoundingClientRect();
+        console.log(height);
 
-    // Width of the wheel well.
-    const LxWell = WheelWidth + Margin;
+        // Size of the viewbox (in meters).
+        const w = width / data.scale;
+        const h = height / data.scale;
 
-    // Length of the wheel well.
-    const LyWell = 2 * (WheelRadius + Margin);
+        // Coordinate of the upper left corner.
+        const x = (data.center.x - (w / 2));
+        const y = (data.center.y - (h / 2));
 
-    // Width of the chassis.
-    const LxChassis = WheelsSpacing + WheelWidth;
+        return `${x} ${y} ${w} ${h}`;
+    });
 
-    // Compute chassis radius.
-    const RChassis = Math.sqrt((LxChassis / 2) * (LxChassis / 2) + (LyWell / 2) * (LyWell / 2));
+    // Distance between center of wheels (m).
+    const WheelsSpacing = 0.25;
 
-    // Append SVG container.
-    const svgContainer = d3.select("body").append("svg")
-        .attr("preserveAspectRatio", "xMinYMin meet")
-        .attr("viewBox", "0 0 3 0.001");
+    // Radius of the wheels (m).
+    const WheelRadius = 0.035;
 
-    // Create a group for the robot.
-    const group = svgContainer.append("g")
-        .attr("transform", "translate(0.5,0.5)");
+    // Generate and append the robot vizualisation to the page body.
+    // const robotSVG = AppendRobot(svg, WheelsSpacing, WheelRadius, 0.02, 0.005, 0.005);
 
-    // Append chassis path (gray).
-    group.append("path")
-        .attr("stroke", "black")
-        .attr("stroke-width", Stroke)
-        .attr("stroke-linejoin", "round")
-        .attr("fill", "lightgray")
-        .attr("d",
-            `M ${-LxChassis / 2} ${-LyWell / 2} ` +
-            `a ${RChassis} ${RChassis} 0 0 1 ${LxChassis} 0 ` +
-            `l ${-LxWell} 0 l 0 ${LyWell} l ${LxWell} 0 ` +
-            `a ${RChassis} ${RChassis} 0 0 1 ${-LxChassis} 0 ` +
-            `l ${LxWell} 0 l 0 ${-LyWell} ` +
-            "Z");
+    // Appends a robot description to the svg.
+    function AppendRobot(svg, WheelsSpacing, WheelRadius, WheelWidth, Margin, Stroke) {
 
-    const appendWheelRectangle = function (x, color) {
-        group.append("rect")
-            .attr("fill", color)
+        // Increase margin to take stroke in consideration.
+        Margin += Stroke;
+
+        // Width of the wheel.
+        const LxWheel = WheelWidth;
+
+        // Length of the wheel.
+        const LyWheel = 2 * WheelRadius;
+
+        // Width of the wheel well.
+        const LxWell = WheelWidth + Margin;
+
+        // Length of the wheel well.
+        const LyWell = 2 * (WheelRadius + Margin);
+
+        // Width of the chassis.
+        const LxChassis = WheelsSpacing + WheelWidth;
+
+        // Compute chassis radius.
+        const RChassis = Math.sqrt((LxChassis / 2) * (LxChassis / 2) + (LyWell / 2) * (LyWell / 2));
+
+
+        // Create a group for the robot.
+        const group = svg.append("g");
+
+        // Append chassis path (gray).
+        group.append("path")
             .attr("stroke", "black")
             .attr("stroke-width", Stroke)
             .attr("stroke-linejoin", "round")
-            .attr("width", LxWheel)
-            .attr("height", LyWheel)
-            .attr("x", x - LxWheel / 2)
-            .attr("y", -LyWheel / 2);
-    };
+            .attr("fill", "lightgray")
+            .attr("d",
+                `M ${-LxChassis / 2} ${-LyWell / 2} ` +
+                `a ${RChassis} ${RChassis} 0 0 1 ${LxChassis} 0 ` +
+                `l ${-LxWell} 0 l 0 ${LyWell} l ${LxWell} 0 ` +
+                `a ${RChassis} ${RChassis} 0 0 1 ${-LxChassis} 0 ` +
+                `l ${LxWell} 0 l 0 ${-LyWell} ` +
+                "Z");
 
-    // Append left wheel rectangle (red).
-    appendWheelRectangle(-(LxChassis - LxWheel) / 2, "red");
+        const appendWheelRectangle = function (x, color) {
+            group.append("rect")
+                .attr("fill", color)
+                .attr("stroke", "black")
+                .attr("stroke-width", Stroke)
+                .attr("stroke-linejoin", "round")
+                .attr("width", LxWheel)
+                .attr("height", LyWheel)
+                .attr("x", x - LxWheel / 2)
+                .attr("y", -LyWheel / 2);
+        };
 
-    // Append right wheel rectangle (blue).
-    appendWheelRectangle((LxChassis - LxWheel) / 2, "blue");
+        // Append left wheel rectangle (red).
+        appendWheelRectangle(-(LxChassis - LxWheel) / 2, "red");
 
-    return group;
+        // Append right wheel rectangle (blue).
+        appendWheelRectangle((LxChassis - LxWheel) / 2, "blue");
+
+        return group;
+    }
 }
+
+window.addEventListener('resize', function () {
+
+    // Recompute svg viewbox.
+    update();
+});
+
+update();
+
+
+// Place the robot.
+// robotSVG.attr("transform", "translate(1.5,0)");
